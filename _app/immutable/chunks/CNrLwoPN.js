edba0dc77174c8d1f4f14ef7586418e6929e74df
sharpsss <aalexso764@gmail.com>
2025-08-24 22:04:04 +0700

    Updates

---
+++ _app/immutable/chunks/CNrLwoPN.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_d0kt8x?.base??"/project_architecture_template",t=globalThis.__sveltekit_d0kt8x?.assets??s;export{t as a,s as b};
--- _app/immutable/chunks/cs3L8EI2.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1x1hu3i?.base??"/project_architecture_template",e=globalThis.__sveltekit_1x1hu3i?.assets??s;export{e as a,s as b};
