485db22120dc29e31e3abe6c413d6303b658807f
sharpsss <aalexso764@gmail.com>
2025-08-24 22:17:36 +0700

    Updates

---
+++ _app/immutable/chunks/CGmblgsE.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_f6u2n2?.base??"/project_architecture_template",e=globalThis.__sveltekit_f6u2n2?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/entry/start.DL97XIzK.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/mCL_5gME.js";export{o as load_css,r as start};
