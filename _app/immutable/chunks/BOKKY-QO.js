97667f4166d87dcc698a21da3ade5c9af37e9520
sharpsss <aalexso764@gmail.com>
2025-08-24 22:01:57 +0700

    Updates

---
+++ _app/immutable/chunks/BOKKY-QO.js
@@ -0,0 +1,1 @@
+import{e}from"./B5Hc0_3G.js";e();
---
+++ _app/immutable/chunks/a3o26SVB.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_uvs6oq?.base??"/project_architecture_template",e=globalThis.__sveltekit_uvs6oq?.assets??s;export{e as a,s as b};
